011ec7fdb893d409c93462c4f9380d74875a3699
dengmiao <dm132@qq.com>
2017-03-20 13:46:23 +0800

    change activity

--- src/main/java/com/example/coolweather/MainActivity.java
+++ src/main/java/com/example/coolweather/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.coolweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    private int id=0;
+public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
